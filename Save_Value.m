7a9d90141131bbb92b465a90a60fa5b5ae4d57b3
Game <6102nantiwat@gmail.com>
2026-02-06 23:39:28 +0700

    yyyy

--- Save_Value.m
+++ Save_Value.m
@@ -1,5 +1,5 @@
 %% โหลดค่า Parameter
-load('Lab1 Part2 Parameter\')
+load('Lab1 Part2 Parameter\ramp_slope0.1.mat');
 
 sim('MotorXploer.slx');
 
@@ -18,6 +18,6 @@ ylabel('Angular Velocity (rad/s)');
 title('Motor Angular Velocity vs Time');
 grid on;
 hold off;
-filename = 'Lab1 Part2 Data\lap1_ramp' % Save ชื่อไฟล์
+filename = 'Lab1 Part2 Data\Lab1 Part2 Data New\lap1_ramp_slope0.1_1' % Save ชื่อไฟล์
 
 save(filename, "speed","time");
